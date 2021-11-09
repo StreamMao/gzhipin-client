0868648545b59e6b6a52c476e08361a8498ee3e1
StreamMao <maosijun1995@gmail.com>
2021-11-08 22:30:13 -0500

    ajax request module, using axios

---
+++ src/api/ajax.js
@@ -0,0 +1,23 @@
+// 能发送ajax请求的函数模块
+// 函数的返回值是promise对象
+import axios from 'axios'
+
+export default function ajax(url, data={}, type='GET') {
+    if (type==='GET') { // 发送get请求
+        // 拼请求参数串
+        let dataStr = ''
+        Object.keys(data).forEach(key => {
+            dataStr += key + '=' + data[key] + "&"
+        })
+        if (dataStr) {
+            dataStr = dataStr.substring(0, dataStr.length - 1)
+        }
+        //使用axios发送get请求
+        return axios.get(url + '?' + dataStr)
+    } else {// 发送post请求
+        //使用axios发送post请求
+        return axios.post(url, data)
+    }
+    
+    
+}
