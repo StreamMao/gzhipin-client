538c92b0121fd284e150486459b5263f2820b249
StreamMao <maosijun1995@gmail.com>
2021-11-08 22:42:58 -0500

    request  function API module

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+// 包含了n个接口请求的函数模块
+// 函数返回值为：promise
+
+import ajax from "./ajax"
+
+//register接口
+export const reqRegister = (user) => {
+    ajax('/register', user, 'POST')
+}
+//login接口
+export const reqLogin = (user) => {
+    ajax('/login', user, 'POST')
+}
+
+//update user接口
+export const reqUpdateUser = (user) => {
+    ajax('/update', user, 'POST')
+}
