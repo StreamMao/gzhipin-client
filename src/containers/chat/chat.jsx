9dd64ce169652af99a16bcd646f7343ef79f5dfd
StreamMao <maosijun1995@gmail.com>
2022-03-09 23:01:19 -0500

    built the chat.jsx to show the message list

--- src/containers/chat/chat.jsx
+++ src/containers/chat/chat.jsx
@@ -23,18 +23,45 @@ class Chat extends Component {
   };
 
   render() {
+    const {user} = this.props
+    const {users, chatMsgs} = this.props.chat
+    //计算当前聊天的chatId
+    const meId = user._id
+    if(!users[meId]) { //如果还没有获取数据，直接不做任何显示
+      return null
+    }
+    const targetId = this.props.match.params.userid
+    const chatId = [meId, targetId].sort().join('_')
+    //对chatMsgs进行过滤，因为里面包含和所有人的聊天记录
+    const msgs = chatMsgs.filter(msg => msg.chat_id === chatId)
+    //得到目标用户的avatar头像
+    const targetAvatar = users[targetId].avatar
+    const targetIcon = targetAvatar? require(`../../assets/images/${targetAvatar}.png`) : null;
+    
     return (
       <div id="chat-page">
         <NavBar>aa</NavBar>
         <List>
-          <Item thumb={require("../../assets/images/头像1.png")}>你好</Item>
-          <Item thumb={require("../../assets/images/头像1.png")}>你好2</Item>
-          <Item className="chat-me" extra="我">
-            很好
-          </Item>
-          <Item className="chat-me" extra="我">
-            很好2
-          </Item>
+          {msgs.map((msg) => {
+            if (targetId === msg.from) {
+              //对方发给我的消息
+              return (
+                <Item
+                  key={msg._id}
+                  thumb={targetIcon}
+                >
+                  {msg.content}
+                </Item>
+              );
+            } else {
+              //我发给对方的消息
+              return (
+                <Item key={msg._id} className="chat-me" extra="我">
+                  {msg.content}
+                </Item>
+              );
+            }
+          })}
         </List>
         <div className="am-tab-bar">
           <InputItem
@@ -49,4 +76,7 @@ class Chat extends Component {
   }
 }
 
-export default connect((state) => ({ user: state.user }), { sendMsg })(Chat);
+export default connect(
+  state => ({ user: state.user, chat: state.chat }), 
+  { sendMsg }
+)(Chat);
